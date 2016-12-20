d3d0c49a5b50e4c6ca514f146c90d6e2b508bb6d
H. İbrahim Güngör <igungor@gmail.com>
2016-12-20 13:44:14 +0300

    sync: add Preallocate documentation

--- sync/preallocate.go
+++ sync/preallocate.go
@@ -2,6 +2,8 @@ package sync
 
 import "os"
 
+// Preallocate tries to allocate continuous disk space for the given file. If
+// system specific pre-allocation fails, it fills the file with zeroes.
 func Preallocate(f *os.File, size int64) error {
 	err := preallocate(f, size)
 	if err == nil {
