05b5ad785685c3be4dad2bc344b9f1f5f047fc12
Cenk Alti <cenkalti@gmail.com>
2020-09-03 15:47:33 +0300

    add more comments to Config

--- config.go
+++ config.go
@@ -21,12 +21,18 @@ type Config struct {
 	// Username of put.io account.
 	Username string
 	// Password of put.io account.
+	// An OAuth token can be used instead of password.
+	// Token must be prefixed with "token/" (without quotes).
+	// In that case, Username is not required.
 	Password string
-	// Do not make changes on filesystems.
+	// Do not make changes on filesystems. Only calculate what needs to be done.
 	DryRun bool
 	// Sync repeatedly. Pause given duration between syncs.
+	// If value is greater than zero, Sync function does not return on synchronization errors.
+	// However, it retruns on authentication error since there is no point in retrying in this case.
 	Repeat time.Duration
 	// Listen address for HTTP server.
+	// The server has an endpoint for getting the status of the sync operation.
 	Server string
 }
 
